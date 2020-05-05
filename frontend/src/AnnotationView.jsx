18735308a53cb8a497c5fd3c7c9cb2d718e5756d
langhabel <langhabel@users.noreply.github.com>
2020-05-05 12:54:12 +0200

    Add dummy file input

--- frontend/src/AnnotationView.jsx
+++ frontend/src/AnnotationView.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Card, Elevation } from "@blueprintjs/core";
+import { Card, Elevation, FileInput } from "@blueprintjs/core";
 import "./AnnotationView.sass";
 
 function AnnotationView() {
-  return <Card className="annotation-view" elevation={Elevation.ONE} />;
+  return (
+    <Card className="annotation-view" elevation={Elevation.ONE}>
+      <FileInput text="Choose file..." buttonText="Browse your computer" fill />
+    </Card>
+  );
 }
 
 export default AnnotationView;
