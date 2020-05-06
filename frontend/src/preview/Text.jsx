bd4aefc682bb0e00d1c95d4cef48d6532e57ac5d
langhabel <langhabel@users.noreply.github.com>
2020-05-06 11:24:41 +0200

    Add dummy preview text

---
+++ frontend/src/preview/Text.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { EditableText } from "@blueprintjs/core";
+
+const PreviewText = () => {
+  return (
+    <EditableText
+      disabled
+      defaultValue="The anonymization preview will go here"
+    />
+  );
+};
+
+export default PreviewText;
