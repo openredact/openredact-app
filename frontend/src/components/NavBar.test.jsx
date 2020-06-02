88071759c0cb6105f23a3feff55d97db7225f10c
langhabel <langhabel@users.noreply.github.com>
2020-06-02 18:49:01 +0200

    Fix failing test by adding polyglot into context

--- frontend/src/components/NavBar.test.jsx
+++ frontend/src/components/NavBar.test.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import NavBar from "./NavBar";
+import PolyglotContext from "../js/polyglotContext";
 
 it("renders help", () => {
-  const { getByTitle } = render(<NavBar />);
+  const { getByTitle } = render(
+    <PolyglotContext.Provider value={{ t: (x) => x }}>
+      <NavBar />
+    </PolyglotContext.Provider>
+  );
   const help = getByTitle(/help/i);
   expect(help).toBeInTheDocument();
 });
