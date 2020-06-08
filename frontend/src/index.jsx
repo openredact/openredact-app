5d3998d76756d3e51f497174d799dea91ae978ba
langhabel <langhabel@users.noreply.github.com>
2020-06-08 12:07:10 +0200

    Only show focus on tab

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { FocusStyleManager } from "@blueprintjs/core";
 import "./index.css";
 import App from "./components/App";
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 ReactDOM.render(
   <React.StrictMode>
