56858af6a269a91937e890e12ab6c996e1c5cd6a
langhabel <langhabel@users.noreply.github.com>
2020-07-16 08:34:19 +0200

    Make uvicorn watch and reload app

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,4 +30,4 @@ app.add_middleware(
 app.include_router(router, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
