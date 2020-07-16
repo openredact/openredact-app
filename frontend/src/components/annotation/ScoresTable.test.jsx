ac92e7f940e594b80d1bee406ab957de51fe8948
langhabel <langhabel@users.noreply.github.com>
2020-07-16 09:48:25 +0200

    Fix test

--- frontend/src/components/annotation/ScoresTable.test.jsx
+++ frontend/src/components/annotation/ScoresTable.test.jsx
@@ -4,9 +4,9 @@ import ScoresTable from "./ScoresTable";
 
 it("renders", () => {
   const { getByText } = render(<ScoresTable scores={{ total: { f2: 1.0 } }} />);
-  const tag = getByText(/total/);
+  const tag = getByText(/total/i);
   expect(tag).toBeInTheDocument();
-  const metric = getByText(/f2/);
+  const metric = getByText(/f2/i);
   expect(metric).toBeInTheDocument();
   const value = getByText("1.00");
   expect(value).toBeInTheDocument();
