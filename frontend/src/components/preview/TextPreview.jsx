746990250234931118352ec5ea60d93607ced650
langhabel <langhabel@users.noreply.github.com>
2020-07-20 13:31:19 +0200

    Visually frame preview such that it appears unalterable
    Closes #13

--- frontend/src/components/preview/TextPreview.jsx
+++ frontend/src/components/preview/TextPreview.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { EditableText } from "@blueprintjs/core";
+import { Card, EditableText, Elevation } from "@blueprintjs/core";
 import PropTypes from "prop-types";
+import "./TextPreview.sass";
 
 const TextPreview = ({ text }) => {
   return (
-    <EditableText
-      disabled
-      multiline
-      className="preview-text"
-      placeholder=""
-      value={text}
-    />
+    <Card elevation={Elevation.THREE} className="document-outline">
+      <EditableText
+        disabled
+        multiline
+        className="preview-text"
+        placeholder=""
+        value={text}
+      />
+    </Card>
   );
 };
 
