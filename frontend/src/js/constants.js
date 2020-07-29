00bbe9fa8f8ba8f8716ab552e223993bb1dd81da
langhabel <langhabel@users.noreply.github.com>
2020-07-29 12:35:36 +0200

    Increase threshold for different tag colors

--- frontend/src/js/constants.js
+++ frontend/src/js/constants.js
@@ -2,7 +2,7 @@ const constants = {};
 
 constants.title = "OpenRedact";
 constants.tooltipHoverOpenDelay = 500;
-constants.maxTagColors = 5;
+constants.maxTagColors = 8;
 constants.previewPdfWidth = 750;
 constants.compileTimeout = 300;
 
